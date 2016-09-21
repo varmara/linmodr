61aeb2f5400c520536d60eeddc62eec362dd33e8
polydora <polydora@rambler.ru>
2016-09-21 19:10:10 +0300

    20.09.16

--- 03_code_LM.R
+++ 03_code_LM.R
@@ -1,86 +1,76 @@
-# "Введение в регрессионный анализ"
-# Вадим Хайтов
-# Каф. Зоологии беспозвоночных, СПбГУ
-# Линейные модели на R, осень 2015
+######################################
+#Введение в регрессионый анализ
+#Линейные модели на R
+#Вадим Хайтов, Марина Варфоломеева
+######################################
 
 
-# Вам понадобятся пакеты ggplot2, ppcor
-# install.packages("ggplot2")
-# install.packages("ppcor")
-
-options(digits=3)
-brain <- read.csv("IQ_brain.csv", header = T)
-
-#Здесь должен появиться Ваш код для корреляционного анализа
+## Читаем данные
+brain <- read.csv("data/IQ_brain.csv", header = TRUE)
+head(brain)
 
 
+## Находим строки с пропущенными значениями
+sum(!complete.cases(brain))
+
+sapply(brain, function(x) sum(is.na(x)))
+
+brain[!complete.cases(brain), ]
 
 
-
-# Вычисляем частные корреляции
-library(ppcor)
-brain_complete <- brain[complete.cases(brain),]
-pcor.test(brain_complete$PIQ, brain_complete$MRINACount, brain_complete$Height, )
+#Вычисление матрицы корреляций
 
 
+## Строим график
+pl_brain <- ggplot(brain, aes(x = MRINACount, y = PIQ)) + geom_point() +   xlab("Brain size") + ylab("IQ test")
+pl_brain
 
-# Подбираем модель для зависимост IQ от размера головного мозга
-brain_model <- lm(PIQ ~ MRINACount, data = brain)
-brain_model
-
-#Здесь должен появиться Ваш код для ответа на следующие вопросы
-# 1. Чему равны угловой коэффициент и свободный член полученной модели brain_model?
-
-
-
-# 2. Какое значеие IQ-теста предсказывает модель для человека с объемом  мозга равным 900000
+## Меняем настройки графика
+pl_brain + theme_dark() +
+  geom_point(aes(color = Gender), size = 4) +
+  scale_color_manual(values =  c("red", "blue"))
 
 
-
-
-# 3. Чему равно значение остатка от модели для человека с порядковым номером 10
+## Подгоняем модель с помощью функции lm()
 
 
 
 
 
+## Прогнозируем величину IQ для человека с размером мозга 900000
 
-# Проводим более детальный анализ модели
-summary(brain_model)
 
-# Графическое представление результатов. Постройте график при разных значениях параметра level
-library(ggplot2)
-pl_brain + geom_smooth(method="lm", level=0.99)
+#Находим остатки
+brain$PIQ[10] - fitted(brain_model)[10]
+residuals(brain_model)[10]
+
+
+##Находим доверительные интервалы для параметров
 
 confint(brain_model)
 
-# Оцениваем ожидаемое значение IQ для человека с заданным размером головного мозга
+##Рисуем графики для разных уровней значимости
+pl_alpha1 <- pl_brain + geom_smooth(method="lm", level=0.8) + ggtitle(bquote(alpha==0.2))
 
+pl_alpha2 <- pl_brain + geom_smooth(method="lm", level=0.95) + ggtitle(bquote(alpha==0.05))
+
+pl_alpha3 <- pl_brain + geom_smooth(method="lm", level=0.999) + ggtitle(bquote(alpha==0.01))
+
+
+grid.arrange(pl_alpha1, pl_alpha2, pl_alpha3, ncol=3)
+
+
+## Вычисляем 95%  зону предсказания
 newdata <- data.frame(MRINACount = 900000)
 
-predict(brain_model, newdata, interval = "confidence", level = 0.95, se = TRUE)
+predict(brain_model, newdata, interval = "prediction", level = 0.95, se = TRUE)$fit
 
 
-# Отражаем на графике область значений, в которую попадут 95% предсказанных величин IQ
-
-# Подготавлваем данные
+## Строим график с зоной педсказаия
 brain_predicted <- predict(brain_model, interval="prediction")
 brain_predicted <- data.frame(brain, brain_predicted)
 head(brain_predicted)
 
-# Отражаем на графике область значений, в которую попадут 95% предсказанных величин IQ
-pl_brain +
-  geom_smooth(method = "lm", aes(fill = "Conf.interval"), alpha = 0.4) +
-  geom_ribbon(data = brain_predicted,  aes(ymin = lwr, ymax = upr, fill = "Conf. area for prediction"), alpha = 0.2) +
-  scale_fill_manual(name = "Intervals", values = c("green", "gray")) +
-  ggtitle("Confidence interval \n and confidence area for prediction")
-# Комментарии к графику:
-# 1) geom_smooth - Линия регрессии и ее дов. интервал
-# Если мы указываем fill внутри aes() и задаем фиксированное значение - появится соотв. легенда с названием.
-# alpha - задает прозрачность
-# 2) geom_ribbon - Интервал предсказаний создаем при помощи геома ribbon ("лента")
-# Данные берем из другого датафрейма - из brain_predicted
-# ymin и ymax - эстетики геома ribbon, которые задают нижний и верхний край ленты в точках с заданным x (x = MRINACount было задано в ggplot() при создании pl_brain, поэтому сейчас его указывать не обязательно)
-# 3) scale_fill_manual - Вручную настраиваем цвета заливки при помощи шкалы fill_manual.
-# Ее аргумент name - название соотв. легенды, values - вектор цветов
-# 4) ggtitle - Название графика
+pl_brain +  geom_smooth(method = "lm", aes(fill = "Conf.interval"), alpha = 0.4) +
+geom_ribbon(data = brain_predicted,  aes(ymin = lwr, ymax = upr, fill = "Conf. area for prediction"), alpha = 0.2) + scale_fill_manual(name = "Intervals", values = c("green", "gray")) +   ggtitle("Confidence interval \n and confidence area for prediction")
+
---
+++ 04_code_Introduction_to_matrix_algebra.R
@@ -0,0 +1,157 @@
+######################################
+#Краткое введение в матричную алгебру
+#Линейные модели на R
+#Вадим Хайтов, Марина Варфоломеева
+######################################
+
+#Создаем матрицу из вектора
+
+matrix(1:12, ncol = 3)
+
+# Единичная матрица
+diag(rep(1,5))
+
+
+# Создаем искусственный датасет для демонстрации сложения матриц
+Large <- data.frame(Sp1 = round(rnorm(5, 10, 2)), Sp2 = round(rnorm(5, 10, 3)), Sp3 = round(rnorm(5, 10, 2)))
+
+rownames(Large) <- c("Sample1", "Sample2", "Sample3", "Sample4", "Sample5" )
+
+Small <- data.frame(Sp1 = round(rnorm(5, 50, 5)), Sp2 = round(rnorm(5, 50, 5)), Sp3 = round(rnorm(5, 50, 5)))
+
+rownames(Small) <- c("Sample1", "Sample2", "Sample3", "Sample4", "Sample5" )
+
+Large
+Small
+
+
+Large + Small
+
+
+##Скалярное произведеине
+
+Rpocessed_portion <- c(1, 1, 1/2, 1/3, 1/4)
+Processed_Factor <- 1/Rpocessed_portion
+
+Small * Processed_Factor
+
+#Матричное произведение двух вектров
+N <- c(20, 40, 32, 45, 80, 50, 10)
+Fert <- c( 0,  0,   1,   2,   2,   0,   0)
+
+t(N) %*% (Fert)
+
+#Матричные произведения
+
+
+
+
+## Демографическая модель с использованием матриц Лесли
+
+T1 <- c(20, 40, 32, 45, 80, 50, 10)
+Age <- c("0", "1-10", "11-20", "21-35", "36-45", "46-55", "56-65")
+Pop <- data.frame(Age, T1)
+
+Lesl <- matrix(
+c( 0,  0,   1,   2,   2,   0,   0,
+  0.6, 0,   0,   0,   0,   0,   0,
+   0,  0.7, 0,   0,   0,   0,   0,
+   0,  0,   0.8, 0,   0,   0,   0,
+   0,  0,   0,   0.7, 0,   0,   0,
+   0,  0,   0,   0,   0.6, 0,   0,
+   0, 0,    0,   0,   0,  0.2, 0  ),
+byrow = T,
+ncol = 7)
+
+Lesl
+
+Pop$T2 <- as.vector( Lesl %*% (Pop$T1 ))
+Pop$T3 <- as.vector( Lesl %*% (Pop$T2 ))
+Pop$T4 <- as.vector( Lesl %*% (Pop$T3 ))
+Pop$T5 <- as.vector( Lesl %*% (Pop$T4 ))
+Pop$T6 <- as.vector( Lesl %*% (Pop$T5 ))
+Pop$T7 <- as.vector( Lesl %*% (Pop$T6 ))
+Pop$T8 <- as.vector( Lesl %*% (Pop$T7 ))
+Pop$T9 <- as.vector( Lesl %*% (Pop$T8 ))
+Pop$T10 <- as.vector( Lesl %*% (Pop$T9 ))
+
+library(ggplot2)
+library(reshape)
+Pop2 <- melt(Pop)
+ggplot(Pop2, aes(x=Age, y = value)) + geom_bar(stat = "identity") + facet_wrap(~variable, ncol = 2)
+
+#Вычисляем матрицу корреляций с помощью матричной алгебры
+
+
+brain <- read.csv("data/IQ_brain.csv", header = TRUE)
+br <- brain[complete.cases(brain), -1]
+br <- as.matrix(br)
+br_scaled <- scale(br) #Стандартизация данных
+
+cor_matrix <- t(br_scaled) %*% br_scaled / (nrow(br_scaled) - 1)
+
+cor_matrix
+
+# Решение систем линейных уравнений с помощью матричной алгебры
+
+
+
+
+
+
+
+##Строим линию регрессии  с помощью средств ggplot2
+
+data(cars)
+Mod <- lm(dist ~ speed, data = cars)
+coefficients(Mod)
+
+library(ggplot2)
+theme_set(theme_bw())
+ggplot(cars, aes(x = speed, y = dist)) + geom_point() + geom_smooth(method = "lm")
+
+
+##Строим линию регрессии вручную
+
+X <- model.matrix(~speed, data = cars)
+Y <- cars$dist
+betas <- solve(t(X) %*% X) %*% (t(X) %*% Y)
+betas
+
+
+predict_values <- X %*% betas
+
+resid_values <- cars$dist - predict_values
+
+s2 <- sum(resid_values^2)/(length(resid_values) - length(betas))
+
+covbetas <- s2 * solve(t(X) %*% X)
+
+covbetas
+
+MyData <- data.frame(speed = seq(min(cars$speed), max(cars$speed)))
+head(MyData)
+
+X <- model.matrix( ~ speed, data = MyData)
+head(X)
+
+
+MyData$predicted <- X %*% betas
+
+MyData$se <- sqrt(diag(X %*% covbetas %*% t(X)))
+
+MyData$CiUp  <- MyData$predicted + 1.96 *MyData$se
+
+MyData$CiLow  <- MyData$predicted - 1.96 *MyData$se
+
+ggplot(MyData, aes(x = speed, y = predicted)) +
+  geom_line(aes(x = speed, y = CiUp),
+            linetype = 2, size = 1) +
+  geom_line(aes(x = speed, y = CiLow),
+            linetype = 2, size = 1)+
+  geom_abline(slope = betas[2], intercept = betas[1],
+              color = "blue", size=2) +
+  geom_point(data = cars, aes(x = speed, y = dist)) +
+  ylab("dist")
+
+
