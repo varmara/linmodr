673101518120b3557b124aa591e057733eafeeb5
Marina Varfolomeeva <marina.varfolomeeva@gmail.com>
2020-12-20 22:26:30 +0300

    typos

--- 14_GLM_binary_code.R
+++ 14_GLM_binary_code.R
@@ -1,139 +1,198 @@
 # "Регресионный анализ для бинарных данных"
 # Вадим Хайтов, Марина Варфоломеева
 # Каф. Зоологии беспозвоночных, СПбГУ
-# Линейные модели на R, осень 2015
-
-
-liz <- read.csv("data/polis.csv")
+# Линейные модели на R, осень 2019
 
 
 
-Mod_1 <- lm(PA ~ PARATIO, data = liz)
-summary(Mod_1)
+## Читаем данные
+
+astr <- read.csv('data/aster_mussel.csv', header = TRUE)
+head(astr)
+
+
+astr$Box <- factor(astr$Box)
+
+
+## Знакомимся с данными
+
+# Нет ли пропущенных значений?
+
+colSums(is.na(astr))
+
+# Каковы объемы выборок?
+
+table(astr$Box)
+
+## Нет ли коллинеарности
+
+library(ggplot2)
+library(cowplot)
+
+Pl_Sp <- ggplot(astr, aes(x = Sp, y = L)) + geom_boxplot()
+Pl_Box <- ggplot(astr, aes(x = Box, y = L)) + geom_boxplot()
+plot_grid(Pl_Sp, Pl_Box, ncol = 2)
+
+
+## Есть ли выбросы?
+
+ggplot(astr, aes(y = 1:nrow(astr))) + geom_point(aes(x = L) )
+
+
+## Кодирование бинарной переменной
+
+astr$Out <- ifelse(test = astr$Outcome == 'eaten', yes = 1,  no = 0)
+
+# Задание
+# Постройте и визуализируйте glm, основаную на нормальном распределении
+
+mod_norm <- ( ~  , data = astr)
+
+
+# Визуализаця в зависимости от Sp, Box и L
+library(dplyr)
+new_data <- astr %>% group_by( , )%>%
+  do(data.frame(L = seq(min(.$L), max(.$L), length.out = 100)))
+
+new_data$fit <- (mod_norm,  = new_data) # Предсказанные значения
+
+ggplot(new_data, aes(x = L, y = fit)) +
+  geom_line(aes(group = Box)) + facet_wrap(~ Sp, ncol = 2) +
+  geom_point(data = astr, aes(x = L, y = Out), size = 0.5, color = 'blue')
+
+# Что смущает?
+
+
+mod_norm_diag <- fortify(mod_norm)
+ggplot(mod_norm_diag, aes(x = .fitted, y = .stdresid)) +
+  geom_point() + geom_vline(xintercept = 0)
 
 
 
+# Строим полную модель
 
-liz_model <- glm(PA ~ PARATIO , family="binomial", data = liz)
-summary(liz_model)
-
-
--2*logLik(liz_model)
-nul_model <-update(liz_model, .~.-PARATIO)
-
--2*logLik(nul_model)
-
-## Задание. Вычислите вручную значение критерия G2 для модели, описывающей встречаемость ящериц (`liz_model`) и оцените уровень значимости для него
+mod <- glm(Out ~ Sp*L*Box, family = binomial(link = 'logit'), data = astr)
 
 
-#Остаточная девианса
-Dev_resid <- -2*logLik(liz_model)
+# Задание
+# Оцените, какая связывающая функция лучше подойдет для этой модели
 
 
-#Нулевая девианса
-Dev_nul <- -2*logLik(nul_model)
 
-# Значение критерия G2
-
-
-G2 <-Dev_nul - Dev_resid
-
-
-p_value <- 1 - pchisq(G2, df = 1)
-
-anova(liz_model, nul_model, test = "Chi")
+## Анализ девиансы для полной модели
 
 library(car)
-Anova(liz_model)
+Anova(mod)
 
-exp(coef(liz_model)[2])
-
-## Построение логистической кривой средствами ggplot
-
-
-
-
-ggplot(liz, aes(x=PARATIO, y=PA)) + geom_point() + geom_smooth(method="glm", method.args = list( family="binomial"), se=TRUE, size = 2) + ylab("Вероятность встречи ящериц")
-
-
-
-##Задание: Постройте график логистической ререссии для модели `liz_model`  без использования `geom_smooth()`
-
-MyData <- data.frame(PARATIO = seq(min(liz$PARATIO),
-                                   max(liz$PARATIO),
-                                   length.out = 100))
-
-
-
-
-MyData$Predicted <- predict(liz_model,
-                            newdata = MyData, type = "response" )
-predict.glm()
-
-ggplot(MyData, aes(x = PARATIO, y = Predicted)) + geom_line()
-
-
-
-
-
-# Формируем модельную матрицу для искуственно созданных данных
-X <- model.matrix( ~ PARATIO, data = MyData)
-
-
-# Вычисляем параметры подобранной модели и ее матрицу ковариаций
-betas    <- coef(liz_model) # Векор коэффицентов
-Covbetas <- vcov(liz_model) # Ковариационная матрица
-
-# Вычисляем предсказанные значения, перемножая модельную матрицу на вектор
-
-# коэффициентов
-MyData$eta <- X %*% betas
-
-qplot( MyData$eta, MyData$Predicted)
-
-## Получаем предсказанные значения
-
-# Переводим предсказанные значения из логитов в вероятности
-MyData$Pi  <- exp(MyData$eta) / (1 + exp(MyData$eta))
-
-## Вычисляем границы доверительного интервала
-
-MyData$se <- sqrt(diag(X %*% Covbetas %*% t(X)))
-
-# Вычисляем доверительные интервалы
-MyData$CiUp  <- exp(MyData$eta + 1.96 *MyData$se ) /
-  (1 + exp(MyData$eta  + 1.96 * MyData$se))
-
-MyData$CiLow  <- exp(MyData$eta - 1.96 *MyData$se ) /
-  (1 + exp(MyData$eta  - 1.96 * MyData$se ))
-
-
-## Строим график
-
-ggplot(MyData, aes(x = PARATIO, y = Pi)) +
-  geom_line(aes(x = PARATIO, y = CiUp),
-            linetype = 2, size = 1) +
-  geom_line(aes(x = PARATIO, y = CiLow),
-            linetype = 2, size = 1) +
-  geom_line(color = "blue", size=2) +
-  ylab("Вероятность встречи")
+#
+# Эту модель можно упростить!
+# Упростите ее
 
 
 
 
 
 
-# Проверка на избыточность дисперсии
 
-E <- resid(liz_model, type = "pearson") #Пирсоновские остатки
-p <- length(coef(liz_model)) #число параметров в модели
-df <-   nrow(model.frame(liz_model)) - p #число степеней свободы
-Overdisp <- sum(E^2) / df
+## Сравните AIC для финальной и полной модели
 
-Overdisp
 
-summary(liz_model)
 
+
+
+
+
+# Диагностика финальной модели
+
+mod6_diag <- data.frame(.fitted = fitted(mod6, type = 'response'),
+                        .resid_p = resid(mod6, type = 'pearson'))
+
+ggplot(mod6_diag, aes(y = .resid_p, x = .fitted)) + geom_point() +
+  geom_hline(yintercept = 0) +  geom_smooth(method = 'loess')
+
+
+
+# Проверка на сверхдисперсию
+
+# Функция для проверки наличия сверхдисперсии в модели (автор Ben Bolker)
+# http://bbolker.github.io/mixedmodels-misc/glmmFAQ.html
+# Код модифицирован, чтобы учесть дополнительный параметр в NegBin GLMM, подобранных MASS::glm.nb()
+overdisp_fun <- function(model) {
+  rdf <- df.residual(model)  # Число степеней свободы N - p
+  if (any(class(model) == 'negbin')) rdf <- rdf - 1 ## учитываем k в NegBin GLMM
+  rp <- residuals(model,type='pearson') # Пирсоновские остатки
+  Pearson.chisq <- sum(rp^2) # Сумма квадратов остатков, подчиняется Хи-квадрат распределению
+  prat <- Pearson.chisq/rdf  # Отношение суммы квадратов остатков к числу степеней свободы
+  pval <- pchisq(Pearson.chisq, df=rdf, lower.tail=FALSE) # Уровень значимости
+  c(chisq=Pearson.chisq,ratio=prat,rdf=rdf,p=pval)        # Вывод результатов
+}
+
+
+overdisp_fun(mod6)
+
+summary(mod6)
+
+# Визуализация модели
+
+library(dplyr)
+new_data <- astr %>% group_by(Sp) %>%
+  do(data.frame(L = seq(min(.$L), max(.$L), length.out = 100)))
+
+## Предсказания модели при помощи операций с матрицами
+
+# Модельная матрица и коэффициенты
+X <- model.matrix(~ Sp + L, data =  new_data)
+b <- coef(mod6)
+
+# Предсказанные значения и стандартные ошибки...
+# ...в масштабе функции связи (логит)
+
+new_data$fit_eta <- X %*% b
+new_data$se_eta <- sqrt(diag(X %*% vcov(mod6) %*% t(X)))
+
+# ...в масштабе отклика (применяем функцию, обратную функции связи)
+
+logit_back <- function(x) exp(x)/(1 + exp(x)) # обратная логит-трансформация
+
+
+new_data$fit_pi <- logit_back(new_data$fit_eta)
+
+new_data$lwr_pi <- logit_back(new_data$fit_eta - 2 * new_data$se_eta)
+new_data$upr_pi <- logit_back(new_data$fit_eta + 2 * new_data$se_eta)
+
+head(new_data, 2)
+
+
+ggplot(new_data, aes(x = L, y = fit_eta, fill = Sp)) +
+  geom_ribbon(aes(ymin = fit_eta - 2 * se_eta, ymax = fit_eta + 2 * se_eta), alpha = 0.5) +   geom_line(aes(color = Sp))
+
+
+
+
+## Визуализация в шкале вероятностей интуитивно понятнее
+
+ggplot(new_data, aes(x = L, y = fit_pi, fill = Sp)) +
+  geom_ribbon(aes(ymin = lwr_pi, ymax = upr_pi), alpha = 0.5) +
+  geom_line(aes(color = Sp)) +
+  labs(y='Вероятность', title = 'Вероятность быть съеденной')
+
+
+
+
+
+
+
+
+##########################################
+#Самостоятельная работа
+
+##########################################
+
+
+
+
+
+#####################################3
 
 # Множественная логистическая регрессия
 
@@ -160,6 +219,10 @@ Anova(M1)
 drop1(M1, test = "Chi")
 M2 <- update(M1, .~.-CRE)
 
+
+
+
+# Проверьте получится ли аналогичная модель, если воспользоваться автоматическим алгоритмом
 step(M1, direction = "backward")
 
 
@@ -176,9 +239,9 @@ anova(M15, M1, test = "Chi")
 summary(M15)
 
 
-M15_diagn <- fortify(M15)
+M16_diagn <- fortify(M16)
 
-ggplot(M15_diagn, aes(x = .fitted, y =.stdresid)) + geom_point() + geom_smooth()
+ggplot(M16_diagn, aes(x = .fitted, y =.stdresid)) + geom_point() + geom_smooth()
 
 
 
@@ -188,43 +251,32 @@ ggplot(M15_diagn, aes(x = .fitted, y =.stdresid)) + geom_point() + geom_smooth()
 
 library(dplyr)
 
-M15_diagn$group <- ntile(M15_diagn$.fitted, 10)
+M16_diagn$group <- ntile(M16_diagn$.fitted, 10)
 
-resi_and_fit <- M15_diagn %>%  group_by(group) %>%  summarise(mean_fit = mean(.fitted), mean_res = mean(.stdresid))
+resi_and_fit <- M16_diagn %>%  group_by(group) %>%  summarise(mean_fit = mean(.fitted), mean_res = mean(.stdresid))
 
 qplot(resi_and_fit$mean_fit, resi_and_fit$mean_res ) + geom_smooth()
 
-lm(mean_res ~ mean_fit, data = resi_and_fit )
-
-exp(coef(M15)[3])
 
 
 #Визуализируем предсказания модели, взяв пр этом TYPE == mergency
 
-MyData = expand.grid(AGE = ,
-                     CAN = ,
-                     SYS = ,
-                     LOC = ,
-                     TYP = "Emergency")
+MyData = surviv %>% group_by(CAN,  TYP, PH, PCO, LOC) %>% do(data.frame(SYS = seq(min(.$SYS), max(.$SYS), length.out = 100)))
 
-MyData$Predicted <- predict(M15, newdata = MyData, type = )
+MyData$AGE <- mean(surviv$AGE)
 
 
-
-ggplot(MyData, aes(x=, y = Predicted, color = , group = )) + geom_line() + facet_grid(LOC~ CAN, labeller = label_both) + scale_color_gradient(low = "green",  high = "red") + labs(label = list(x = "Давление в момент реанимации (SYS)", y = "Вероятность гибели", color = "Возраст", title = "Предсказания модели"))
+MyData$Predicted <- predict(M16, newdata = MyData, type = "response" )
 
 
 
-##########################################
-#Самостоятельная работа
-
-##########################################
-
-# Загрузите датасет malaria из пакета ISwR и выясните есть ли связь между вероятностью заболевания малярией и возрастом
+ggplot(MyData[MyData$TYP == "Emergency", ], aes(x=SYS, y = Predicted)) + geom_line() + facet_grid(LOC + PH + PCO ~ CAN, labeller = label_both) + scale_color_gradient(low = "green",  high = "red") + labs(label = list(x = "Давление в момент реанимации (SYS)", y = "Вероятность гибели", title = "Предсказания модели")) + theme_bw()
 
 
-library(ISwR)
-data("malaria")
+
+# Задание для самостоятельной работы
+# Зависит ливероятность встретить ящериц (PA) от степени изрезанности острова (PARATIO)
+liz <- read.csv("data/polis.csv")
 
 
 
