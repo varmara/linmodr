63229aeeb9915a2e0b39345bae9eca04aba49da6
Marina Varfolomeeva <marina.varfolomeeva@gmail.com>
2021-10-06 21:17:42 +0300

    before hypothesis testing

--- 04_hypothesis_testing_code.R
+++ 04_hypothesis_testing_code.R
@@ -2,14 +2,106 @@
 # title: "Тестирование статистических гипотез"
 # author: "Марина Варфоломеева, Юта Тамберг, Вадим Хайтов"
 
+# # Центральная предельная теорема (демонстрация) #########################
 
-# # Доверительный интервал
+library(ggplot2)
+theme_set(theme_bw())
+data("diamonds")
 
+# 1. Среднее в генеральной совокупности -----------------------------------
+
+mu_population <- mean(diamonds$price)
+
+# График распределения цен алмазов в генеральной совокупности.
+# Отмечено среднее
+gg_population <- ggplot(data = diamonds, aes(x = price)) +
+  geom_histogram(fill = 'lightskyblue1', colour = 'black') +
+  geom_vline(aes(xintercept = mu_population), colour = 'red', size = 2)
+gg_population
+
+# 2. Выборочное среднее ---------------------------------------------------
+
+# Функция, которая берет выборку из вектора population размером size,
+# затем строит график распределения значений в выборке.
+# Отмечены среднее в генеральной совокупности и среднее в выборке.
+# (NA предварительно удалены из вектора population)
+gg_sample_hist <- function(population, size, mu) {
+  mu_population <- mean(na.omit(population))
+  id <- sample(x = length(na.omit(population)), size = size)
+  subsample <- population[id]
+  my_mean <- mean(subsample)
+  my_dat <- data.frame(subsamp = subsample)
+  ggplot(data = my_dat, aes(x = subsamp)) +
+    geom_histogram(binwidth = 50, fill = 'grey40', colour = 'grey40') +
+    geom_vline(aes(xintercept = mu_population), colour = 'red', size = 2) +
+    geom_vline(aes(xintercept = my_mean), colour = 'yellow3', size = 2) +
+    coord_cartesian(xlim = c(-1, max(subsample))) +
+    annotate('text', x = Inf, y = Inf, hjust = 1, vjust = 1.5,
+             label = paste('n ==', size),
+             parse = T, size = 3.5) +
+    annotate('text', x = Inf, y = Inf, hjust = 1, vjust = 3.5,
+             label = paste('bar(x) ==', format(my_mean, nsmall = 2, digits = 2)),
+             parse = T, size = 3.5, colour = "yellow4") +
+    annotate('text', x = Inf, y = Inf, hjust = 1, vjust = 4.5,
+             label = paste('mu ==', mu_population),
+             parse = T, size = 3.5, colour = "red")
+}
+
+# График распределения цен алмазов в выборке
+set.seed(24)
+gg_sample_hist(population = diamonds$price, size = 20)
+
+# 3. Распределение выборочных средних -------------------------------------
+
+# Функция, которая берет выборку объемом size из вектора x
+# и возвращает ее среднее значение.
+# (NA предварительно удалены из вектора x)
+sample_mean <- function(x, size){
+  x <- na.omit(x)
+  id <- sample(x = length(x), size)
+  my_mean <- mean(x[id])
+  return(my_mean)
+}
+
+# Функция, которая берет несколько (n_samples) выборок
+# объемом size из вектора x и строит распределение выборочных средних.
+# Отмечено среднее в генеральной совокупности (красная линия)
+# и среднее значение в распределении выборочных средних.
+gg_sample_means <- function(population, n_samples, size){
+  # Считаем средние значения для большого числа выборок
+  mu_population <- mean(na.omit(population))
+  my_means <- replicate(n = n_samples, expr = sample_mean(population, size))
+  mean_of_means <- mean(my_means)
+  sd_of_means <- sd(my_means)
+  my_dat <- data.frame(means = my_means)
+  ggplot(data = my_dat, aes(x = means)) +
+    geom_histogram(binwidth = 4, fill = 'yellow3', alpha = 0.3, color = 'black') +
+    geom_vline(xintercept = mu_population, colour = 'red', size = 3) +
+    geom_vline(xintercept = mean_of_means, colour = 'gold1', size = 1.5) +
+    coord_cartesian(xlim = c(-1, max(my_means))) +
+    annotate('text', x = Inf, y = Inf, hjust = 1, vjust = 1.5,
+             label = paste('n ==', size),
+             parse = T, size = 3.5) +
+    annotate('text', x = Inf, y = Inf, hjust = 1, vjust = 3.5,
+             label = paste('bar(x) ==', format(mean_of_means, nsmall = 2, digits = 2)),
+             parse = T, size = 3.5, colour = "yellow4") +
+    annotate('text', x = Inf, y = Inf, hjust = 1, vjust = 4.5,
+             label = paste('mu ==', mu_population), colour = "red",
+             parse = T, size = 3.5) +
+    annotate('text', x = Inf, y = Inf, hjust = 1, vjust = 8.5,
+             label = paste('~~~sd ==', format(sd_of_means, nsmall = 2, digits = 2)),
+             parse = T, size = 3.5, colour = "yellow4")
+}
+
+# График распределения выборочных средних
+gg_sample_means(population = diamonds$price, n_samples = 500, size = 2)
+
+
+
+# # Доверительный интервал ##########################################
 
 
 # ## Расчет и изображение доверительного интервала в R ##############
-library(ggplot2)
-data("diamonds")
 
 # цена бриллиантов хорошего качества огранки
 good <- diamonds$price[diamonds$cut == "Good"]
@@ -25,7 +117,6 @@ err <- t_crit * SE                   # предел погрешности
 
 
 # ## Строим доверительные интервалы в ggplot
-theme_set(theme_bw())
 
 ggplot(data = diamonds, aes(x = cut, y = price)) +
   stat_summary(geom = 'pointrange', fun.data = mean_cl_normal)
@@ -36,11 +127,24 @@ ggplot(data = diamonds, aes(x = cut, y = price)) +
 #
 # - качество огранки (`cut`) идеальное (`Ideal`)
 # - прозрачность (`clarity`) наивысшая (`IF`)
-#
-# Постройте график средней цены с доверительными интервалами для бриллиантов разного качества огранки, и прозрачности.
 
+# нужные бриллианты
+group <- diamonds$price[ ]
 
+.mean <- mean(group)                  # выборочное среднее
+.n <- length(group)                   # объем выборки
+SE <- sd(group)/ sqrt(.n)             # стандартная ошибка
+t_crit <- qt() # критич. зн. t
+err <- t_crit * SE                   # предел погрешности
+# Границы доверительного интервала
+.mean + c(-1, 1) * err
 
+# Постройте один общий график средней цены с доверительными интервалами
+# для бриллиантов разного качества огранки, и прозрачности.
+
+ggplot(data = , aes(x = , y = ,  = clarity)) +
+   (geom = '', fun.data = ) +
+  facet_ (~ clarity)
 
 
 ## Одновыборочный $t$-тест ################################################
@@ -51,14 +155,23 @@ ggplot(data = diamonds, aes(x = cut, y = price)) +
 # Отличается ли реальная плодовитость в обследованной вами популяции черепах от того, что указано в статье?
 
 mu <- 7
-X <- c(5.7, 11.6, 5.7, 9.89, 9.01, 10.1, 12.4, 2.37, 7.21, 6.86, 9.77, 10.6, 3.82, 13.7, 6.01, 10.2, 7.36, 14.8, 10.1, 8.44, 10.5, 8.85, 4.18, 9.12, 12.3, 7.83, 9.51, 4.01, 9.75, 7.59, 0.769, 8.5, 10.8)
+X <- c(5.7, 11.6, 5.7, 9.89, 9.01, 10.1, 12.4, 2.37, 7.21, 6.86, 9.77,
+       10.6, 3.82, 13.7, 6.01, 10.2, 7.36, 14.8, 10.1, 8.44, 10.5,
+       8.85, 4.18, 9.12, 12.3, 7.83, 9.51, 4.01, 9.75, 7.59, 0.769,
+       8.5, 10.8)
 (n <- length(X)) # всего черепах
 (x <- mean(X))   # средний размер черепах в выборке
 (s <- sd(X))     # стандартное отклонение в выборке
 
 
-
-
+### Задание 2 ------------------------------------------------------------
+# Проверьте условия применимости t-теста. Вычислите t и p.
+# t = (наблюдаемое - ожидаемое) / (станд.ошибка наблюдаемого)
+library()
+qqPlot()
+t <-
+df <-
+p <-
 
 
 # # Двухвыборочный t-тест ####################################################
@@ -82,45 +195,52 @@ X <- c(5.7, 11.6, 5.7, 9.89, 9.01, 10.1, 12.4, 2.37, 7.21, 6.86, 9.77, 10.6, 3.8
 library(MASS)
 data("Cushings")
 
+head(Cushings)
+str(Cushings)
 
-tt <- t.test(formula = Tetrahydrocortisone ~ Type, data = Cushings,
-             subset = Cushings$Type %in% c('a', 'b'))
+colSums(is.na(Cushings))
+
+table(Cushings$Type)
+
+qqPlot(Cushings$Tetrahydrocortisone[Cushings$Type == 'a'])
+qqPlot(Cushings$Tetrahydrocortisone[Cushings$Type == 'b'])
+
+tt <- t.test(x = Cushings$Tetrahydrocortisone[Cushings$Type == 'a'],
+             y = Cushings$Tetrahydrocortisone[Cushings$Type == 'b'])
 tt
 
-# Задание 2------------------------------------------------------------------
-# Перепишите вызов функции t.test с использованием
-# другого шаблона вызова (с параметрами x и y)
 
 
 # Задание 3------------------------------------------------------------------
-# Как называются отдельные элементы результатов
-# можно узнать посмотрев их структуру при помощи
-# функции `str()`
-
+# Перепишите вызов функции t.test с использованием
+# другого шаблона вызова (с использованием формулы).
+tt <- t.test(formula =  ~ , data = Cushings,
+             subset = Cushings$Type  c('a', 'b'))
+tt
 
 # Задание 4------------------------------------------------------------------
-# ## Получите отдельные элементы результатов из
-# объекта tt при помощи оператора $
+# Посмотрите структуру результатов (`tt`) при помощи
+# функции `str()` и извлеките из них:
+# - степени свободы
+# - уровень значимости
 # - значение t-критерия
 
-# - число степеней свободы
-
-# - уровень значимости
 
 
 # ## Задание 5 ----------------------------------------------------------------
-#
-# Файл `aml.csv` содержит данные о влиянии регулярной химиотерапии на продолжительность ремиссии.
-#
+
+# Файл `aml.csv` содержит данные о влиянии регулярной химиотерапии
+# на продолжительность ремиссии.
 # Прочитаем эти данные
 rem <- read.csv(file = "data/aml.csv", header = TRUE)
 str(rem)
-
 # - В переменной `time` представлена продолжительность ремиссии в днях.
-# - `group` указывает, к какой экспериментальной группе принадлежал пациент. В группе 1 проводилась регулярная химиотерапия, в группе 2 - нет.
+# - `group` указывает, к какой экспериментальной группе принадлежал пациент.
+# В группе 1 проводилась регулярная химиотерапия, в группе 2 - нет.
 #
 # - Сравните эти группы с помощью t-теста.
-# - Постройте график со средними и доверительными интервалами для продолжительности ремиссии в этих группах.
+# - Постройте график со средними и доверительными интервалами
+# для продолжительности ремиссии в этих группах.
 
 
 
