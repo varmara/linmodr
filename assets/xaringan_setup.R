3521b6c89523f6ac279d2f0bc6e9ad03c38d6ad1
Marina Varfolomeeva <marina.varfolomeeva@gmail.com>
2022-10-11 21:53:10 +0300

    before model description

--- assets/xaringan_setup.R
+++ assets/xaringan_setup.R
@@ -8,7 +8,8 @@ options(width = 70, scipen = 6, digits = 4)
 library(knitr)
 knitr::opts_template$set(
   fig.full = list(fig.width = 12, fig.height = 9),
-  fig.large = list(fig.width = 12, fig.height = 8),
+  fig.large = list(fig.width = 12, fig.height = 7.5),
+  fig.wide.tall = list(fig.width = 12, fig.height = 8),
   fig.wide.taller = list(fig.width = 12, fig.height = 6),
   fig.wide = list(fig.width = 12, fig.height = 4.5),
   fig.wider.tall = list(fig.width = 8, fig.height = 8),
@@ -17,15 +18,20 @@ knitr::opts_template$set(
   fig.medium.tall = list(fig.width = 6, fig.height = 8),
   fig.medium.taller = list(fig.width = 6, fig.height = 6),
   fig.medium = list(fig.width = 6, fig.height = 4.5),
-  fig.small = list(fig.width = 6, fig.height = 4)
+  fig.wider.small = list(fig.width = 8, fig.height = 4),
+  fig.small = list(fig.width = 6, fig.height = 4),
+  fig.wider.smaller = list(fig.width = 8, fig.height = 3.5),
+  fig.smaller = list(fig.width = 6, fig.height = 3.5)
 )
 # chunk default options
 opts_chunk$set(message = FALSE, tidy = FALSE, warning = FALSE, comment = "", opts.label='fig.medium', fig.showtext = TRUE, echo=TRUE)
 
 library("xaringanthemer")
 style_duo_accent(
+  # primary_color = "#9B3C17",
+  # secondary_color = "#649015",
   primary_color = "#9B3C17",
-  secondary_color = "#649015",
+  secondary_color = "#18a780",
   base_font_size = "22px",
   text_font_size = "1rem",
   header_h1_font_size = "2.2rem",
