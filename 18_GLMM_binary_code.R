e642fccea846fddc5c981d63a46351d5a45b3cb9
Vadim Khaitov <polydora@rambler.ru>
2022-11-28 12:40:54 +0300

    открыл презентации

--- 18_GLMM_binary_code.R
+++ 18_GLMM_binary_code.R
@@ -9,11 +9,15 @@
 astr2 <- read.csv('data/aster_mussel_full.csv', header = TRUE)
 head(astr2)
 
+str(astr2)
+
+
 ## Наводим порядок в кодировке переменных
 
 astr2$Year <- factor(astr2$Year)
 astr2$Box <- factor(astr2$Box)
 astr2$Sp <- factor(astr2$Sp)
+astr2$Experiment <- factor(astr2$Experiment)
 astr2$Out <- ifelse(test = astr2$Outcome == 'eaten', yes = 1,  no = 0)
 
 ## Знакомимся с данными
@@ -39,20 +43,20 @@ ggplot(astr2, aes(y = 1:nrow(astr2))) + geom_point(aes(x = L) )
 
 library(lme4)
 
-model1_ri <- glmer(Out ~ L*Sp*Year + (1|Experiment/Box) , data = astr2,
-                   family = binomial(link = "logit"))
+model1_ri <- glmer(Out ~ L*Sp*Year + (1|Experiment/Box) , data = astr2, family = binomial(link = "logit"))
 
 
 
-astr2$L_scaled <-
+astr2$L_scaled <- scale(astr2$L)
+
+qplot(astr2$L, astr2$L_scaled)
 
 model1_ri <- glmer(Out ~ L_scaled*Sp*Year +
                      (1|Experiment/Box) , data = astr2,
                    family = binomial(link = "logit"))
 
 
-model1_rsi_1<- glmer(Out ~ L_scaled * Sp * Year + (1 + Sp|Experiment/Box) ,
-                     data = astr2, family = binomial(link = "logit"))
+# model1_rsi_1<- glmer(Out ~ L_scaled * Sp * Year + (1 + Sp|Experiment/Box), data = astr2, family = binomial(link = "logit"))
 
 model1_rsi_2 <- glmer(Out ~ L_scaled * Sp * Year + (1 + L_scaled |Experiment/Box) ,
                       data = astr2, family = binomial(link = "logit"))
@@ -71,13 +75,17 @@ model1_rsi_1<- glmer(Out ~ L_scaled * Sp * Year + (1 + L_scaled |Experiment/Box)
 
 ## Сравниваем три модели
 
+AIC(model1_ri,model1_rsi_1)
 
 
 
 
 ## Диагностика модели: линейность связи
 library(ggplot2)
-model1_diagn <- fortify(model1_ri)
+
+model1_diagn <- fortify.merMod(model1_ri)
+
+
 ggplot(model1_diagn, aes(x = .fitted, y = .scresid)) + geom_point() + geom_smooth()
 
 
@@ -90,10 +98,15 @@ ggplot(model1_diagn, aes(x = .fitted, y = .scresid)) + geom_point() + geom_smoot
 
 ## Диагностика модели: избыточность дисперсии
 
-library(sjstats)
-overdisp(model1_ri)
+library(performance)
+check_overdispersion(model1_ri)
 
+summary(model1_ri)
 ## Задание: Проведите упрощение модели в соответствии с протоколом backward selection
+
+model1_ri_6 <- glmer(Out ~ L_scaled + Sp +
+                     (1|Experiment/Box) , data = astr2,
+                   family = binomial(link = "logit"))
 
 
 
@@ -103,19 +116,28 @@ overdisp(model1_ri)
 
 
 ## Диагностика финальной модели: линейность связи
-model6_diagn <- fortify(model6)
+model6_diagn <- fortify.merMod(model1_ri_6)
 ggplot(model6_diagn, aes(x = .fitted, y = .scresid)) + geom_point() + geom_smooth()
 
 
 
 ## Диагностика финальной модели: избыточность дисперсии
 
-overdisp(model6)
+check_overdispersion(model1_ri_6)
 
 
 #Случайные эффекты
 
-icc(model6)
+icc(model1_ri_6)
+
+library(partR2)
+partR2(model1_ri_6)
+
+
+
+summary(model1_ri_6)
+
+exp(fixef(model1_ri_6)[3])
 
 
 ## Подготовка к визуализации в виде логистических кривых
@@ -128,10 +150,10 @@ new_data <- astr2 %>% group_by(Sp) %>% do(data.frame(L_scaled = seq(min(.$L_scal
                                                                     length.out = 100)))
 
 X <- model.matrix(~  L_scaled + Sp, data = new_data)
-b <- fixef(model6)
+b <- fixef(model1_ri_6)
 
-new_data$fit_eta <- X %*% b
-new_data$se_eta <- sqrt(diag(X %*% vcov(model6) %*% t(X)))
+new_data$fit_eta <- as.numeric(X %*% b)
+new_data$se_eta <- sqrt(diag(X %*% vcov(model1_ri_6) %*% t(X)))
 
 new_data$fit_pi <- logit_back(new_data$fit_eta)
 new_data$lwr <- logit_back(new_data$fit_eta - 2 * new_data$se_eta)
@@ -146,8 +168,26 @@ Pl_log
 
 # Задание: Визуализируйте модель в виде столбчатой диаграммы
 
+new_data <- data.frame(Sp = levels(astr2$Sp), L_scaled = 0)
 
 
+X <- model.matrix(~  L_scaled + Sp, data = new_data)
+b <- fixef(model1_ri_6)
+
+new_data$fit_eta <- as.numeric(X %*% b)
+new_data$se_eta <- sqrt(diag(X %*% vcov(model1_ri_6) %*% t(X)))
+
+new_data$fit_pi <- logit_back(new_data$fit_eta)
+new_data$lwr <- logit_back(new_data$fit_eta - 2 * new_data$se_eta)
+new_data$upr <- logit_back(new_data$fit_eta + 2 * new_data$se_eta)
+
+
+ggplot(new_data, aes(x = Sp, y = fit_pi)) +
+  geom_col(aes(fill = Sp)) +
+  geom_errorbar(aes(ymin = lwr, ymax = upr), width = 0.2) +
+  guides(fill = "none") +
+  labs(x = "Species", y = "Probability of being eaten") +
+  scale_fill_manual(values = c("red", "blue"))
 
 
 
@@ -163,7 +203,10 @@ table(astr2$Size_class, astr2$Sp)
 
 Mean_Out <- astr2 %>% group_by(Size_class, Sp) %>%
   do(data.frame(Out = mean(.$Out), L_scaled = mean(.$L_scaled)))
-Pl_log + geom_point(data = Mean_Out, aes(x = L_scaled, y = Out, color = Sp))
+
+Pl_log +
+  geom_point(data = Mean_Out, aes(x = L_scaled, y = Out, color = Sp)) +
+  geom_point(data = astr2, aes(y = Out, color = Sp), position = position_jitter(height = 0.1))
 
 
 
@@ -179,11 +222,17 @@ model6_unscaled <- glmer(Out ~ L + Sp +
 
 # Задание: Сделайте предсказания для новых данных
 
+astr_test$Predicted <- predict(model6_unscaled, newdata = astr_test, re.form = NA)
+
+astr_test$Predicted_pi <- logit_back(astr_test$Predicted)
 
 
 
 
 # Задание: Предложите способ визуализировать соотношение предсказанных и наблюдаемых значений.
+
+ggplot(astr_test, aes(x = Outcome, y = Predicted_pi, fill = Sp)) +
+  geom_boxplot(notch = T)
 
 
 
@@ -204,7 +253,7 @@ Mod_gh <- glmer(cbind(nGreen, nBrown) ~ Sex + Bio18 + (1|SiteID), data = Grassho
 
 Mod_gh <- glmer(cbind(nGreen, nBrown) ~  Sex + scale(Bio18) + (1|SiteID) , data = Grasshoppers, family = binomial(link = "logit") )
 
-
+library(car)
 vif(Mod_gh)
 
 library(performance)
@@ -242,6 +291,7 @@ qplot(x = fitted(Mod_gh_gam), y = residuals(Mod_gh_gam, type = "pearson")) +
 
 qplot(x = (Grasshoppers$Bio18), y = residuals(Mod_gh_gam, type = "pearson")) + geom_smooth()
 
+qplot(x = fitted(Mod_gh_gam), y = residuals(Mod_gh_gam, type = "pearson")) + geom_smooth() + geom_hline(yintercept = 0)
 
 summary(Mod_gh_gam)
 
@@ -284,6 +334,7 @@ bal <- read.table("data/Yakovis2.csv", header = TRUE, sep = ";")
 #Some housekeeping
 bal$Site <- factor(bal$Site)
 bal$Sample <- factor(bal$Sample)
+bal$Position <- factor(bal$Position)
 bal$Substrate_ID <- factor(bal$Substrate_ID)
 
 
@@ -304,14 +355,36 @@ mean(bal[bal$Status == "empty_test", ]$Drill)
 
 bal2 <- bal[bal$Status == "empty_test", ]
 
+pl <- ggplot(bal2, aes(y = 1:nrow(bal2))) +geom_point()
+pl + aes(x = BorN)
+pl +aes(x = ALength)
+pl + aes(x= Age)
+table(bal2$Position)
+table(bal2$Site)
 
 library(lme4)
-M1_glmer <- glmer(Drill ~     , data = bal2, family = )
+M1_glm <- glm(Drill ~ Position + ALength + Age +  BorN + Site, data = bal2, family = "binomial")
 
+vif(M1_glm)
 
+M2_glm <- glm(Drill ~ Position + ALength +  BorN + Site, data = bal2, family = "binomial")
+vif(M2_glm)
 
+M_glmer <- glmer(Drill ~ Position * scale(ALength) * scale(BorN) + Site + (1|Sample/Substrate_ID), data = bal2, family = "binomial", control=glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e5)))
 
+M_glmer_diag <- fortify.merMod(M_glmer)
+ggplot(M_glmer_diag, aes (x = .fitted, y = .scresid)) + geom_point() + geom_smooth()
 
+ggplot(M_glmer_diag, aes (x = ALength, y = .scresid)) + geom_point() + geom_smooth()
+
+ggplot(M_glmer_diag, aes (x = BorN, y = .scresid)) + geom_point() + geom_smooth(method = "loess")
+
+ggplot(M_glmer_diag, aes (x = Position, y = .scresid)) + geom_boxplot()
+
+ggplot(M_glmer_diag, aes (x = Position, y = .scresid)) + geom_boxplot()
+
+summary(M_glmer)
+model.matrix(M_glmer)
 
 
 ###################################################
